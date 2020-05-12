7fa60d199c899b2f4e9f233fa175d8453153fff8
Alvaro Aleman <alvaroaleman@users.noreply.github.com>
2020-05-12 13:50:34 -0400

    Add script to run the bot

---
+++ hack/run.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+if [[ -z $BOT_TOKEN ]]; then echo "BOT_TOKEN var must be set"; exit 1; fi
+
+tmp_kk=$(mktemp)
+trap 'rm -rf $tmp_kk' EXIT
+
+kubectl config view >$tmp_kk
+kubectl --kubeconfig=$tmp_kk config use-context api.ci
+
+
+kubectl config use-context app.ci
+cd $(dirname $0)/..
+make
+./ci-chat-bot \
+  --force-pr-owner=system:serviceaccount:ci:ci-chat-bot \
+  --job-config ../release/ci-operator/jobs/openshift/release/ \
+  --prow-config ../release/core-services/prow/02_config/_config.yaml \
+  --build-cluster-kubeconfig=$tmp_kk \
+  --v=2
