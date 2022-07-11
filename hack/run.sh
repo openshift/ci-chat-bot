83ec8e368559a05e4ec94ca68347e140b02c6689
Alex Pavel <apavel@redhat.com>
2022-07-11 11:53:05 -0700

    manager: add support for launching arm64 clusters
    This PR adds support for launching arm64 clusters by adding `arm64` to
    the options list.

--- hack/run.sh
+++ hack/run.sh
@@ -3,6 +3,7 @@
 set -euo pipefail
 
 if [[ -z $BOT_TOKEN ]]; then echo "BOT_TOKEN var must be set"; exit 1; fi
+if [[ -z $BOT_APP_TOKEN ]]; then echo "BOT_APP_TOKEN var must be set"; exit 1; fi
 
 tmp_dir=$(mktemp -d)
 tmp_kk=$tmp_dir/build.kubeconfig
@@ -20,4 +21,5 @@ make
   --job-config ../release/ci-operator/jobs/openshift/release/ \
   --prow-config ../release/core-services/prow/02_config/_config.yaml \
   --build-cluster-kubeconfigs-location=$tmp_dir \
+  --workflow-config-path ../release/core-services/ci-chat-bot/workflows-config.yaml
   --v=2
