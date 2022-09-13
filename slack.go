e17f7a9ceee1c3c609063a1aa0d487c01e467f79
Alex Pavel <apavel@redhat.com>
2022-09-13 13:39:02 -0700

    slack: default to `aws` for all architectures

--- slack.go
+++ slack.go
@@ -766,9 +766,7 @@ func parseOptions(options string) (string, string, map[string]string, error) {
 	}
 	if len(platform) == 0 {
 		switch architecture {
-		case "amd64":
-			platform = "gcp"
-		case "arm64", "multi":
+		case "amd64", "arm64", "multi":
 			platform = "aws"
 		default:
 			return "", "", nil, fmt.Errorf("unknown architecture: %s", architecture)
