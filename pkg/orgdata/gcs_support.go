b5decc68fbd54aeae9032a33b1264c08599b4532
Eris Hoxha <ehoxha@redhat.com>
2025-11-21 10:57:45 +0100

    fix SetupGCSDataSource to use updated cyborg-data API

--- pkg/orgdata/gcs_support.go
+++ pkg/orgdata/gcs_support.go
@@ -14,16 +14,24 @@ import (
 // SetupGCSDataSource creates and configures a GCS data source when GCS support is enabled
 func SetupGCSDataSource(ctx context.Context, gcsConfig orgdatacore.GCSConfig, orgDataService orgdatacore.ServiceInterface) error {
 	klog.Infof("Loading organizational data from GCS: gs://%s/%s", gcsConfig.Bucket, gcsConfig.ObjectPath)
+
+	// Create GCS data source with SDK
 	gcsSource, err := orgdatacore.NewGCSDataSourceWithSDK(ctx, gcsConfig)
 	if err != nil {
 		return fmt.Errorf("failed to create GCS data source: %w", err)
 	}
 
+	// Load initial data
 	if err := orgDataService.LoadFromDataSource(ctx, gcsSource); err != nil {
+		// Don't forget to close the client on error
+		if closeErr := gcsSource.Close(); closeErr != nil {
+			klog.Warningf("Failed to close GCS client after load error: %v", closeErr)
+		}
 		return fmt.Errorf("failed to load organizational data from GCS: %w", err)
 	}
 
 	klog.Info("Successfully loaded organizational data from GCS")
+
 	// Start GCS watcher for hot reload
 	go func() {
 		if err := orgDataService.StartDataSourceWatcher(ctx, gcsSource); err != nil {
